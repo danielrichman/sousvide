2695807ca15417e1375b3c0d15c0c136e4d8461d
Daniel Richman <main@danielrichman.co.uk>
2017-08-19 21:40:47 +0000

    x

--- daemon.py
+++ daemon.py
@@ -7,21 +7,29 @@ import psycopg2
 def read_temperature():
     base = "/sys/bus/w1/devices"
     for name in os.listdir(base):
+        if name == "w1_bus_master1":
+            continue
+
         try:
             with open(os.path.join(base, name, "w1_slave")) as f:
-                status = f.readline()
-                if not status.endswith("YES"):
-                    raise Exception("bad status", status)
-                data = f.readline()
-                _, t_equals, value = data.partition("t=")
-                if not t_equals = "t=":
-                    raise Exception("expected t=", data)
-                value = int(value) / 1000.
+                status = f.readline().strip()
+                data = f.readline().strip()
                 if f.readline() != "":
                     raise Exception("expected two lines")
-                yield (name, value)
-        except:
-            pass
+
+                if not status.endswith("YES"):
+                    raise Exception("bad status", status)
+                _, t_equals, value = data.partition("t=")
+                if t_equals != "t=":
+                    raise Exception("expected t=", data)
+
+                value = int(value) / 1000.
+
+        except Exception as exn:
+            print("Failed to read temperature", exn)
+
+        else:
+            yield (name, value)
 
 def cycle():
     db_conn = psycopg2.connect(dbname="sousvide")
@@ -31,10 +39,16 @@ def cycle():
     for temp_name, temp_value in read_temperature():
         print("Read temperature:", temp_name, temp_value)
         db_cur.execute("INSERT INTO temperatures (sensor_name, time, reading) \
-                        VALUES (%s, current_time, %s)",
+                        VALUES (%s, current_timestamp, %s)",
                         (temp_name, temp_value))
+
+    db_cur.close()
+    db_conn.close()
 
 def main():
     while True:
         cycle()
         time.sleep(1)
+
+if __name__ == "__main__":
+    main()
