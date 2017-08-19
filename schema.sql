d33702f528085f9287c6254fec7307b92bf1c978
Daniel Richman <main@danielrichman.co.uk>
2017-08-19 19:56:24 +0100

    x

--- schema.sql
+++ schema.sql
@@ -1,19 +1,18 @@
 CREATE ROLE "www-data" LOGIN;
 CREATE ROLE "sousvide" LOGIN;
 CREATE ROLE "daniel" LOGIN;
+CREATE ROLE "chef";
+GRANT "chef" TO "daniel";
 
 CREATE DATABASE "sousvide";
 \c sousvide
 
-CREATE TABLE cooking (
+CREATE TABLE orders (
     seqno serial primary key,
     name text not null,
     start_time timestamp with time zone not null,
     end_time timestamp with time zone not null,
 
-    last_updated timestamp with time zone,
-    current_duty_cycle double precision,
-    
     CONSTRAINT end_after_start CHECK ( end_time >= start_time ),
     CONSTRAINT interval_size CHECK ( end_time - start_time < interval '3 hours' )
 );
@@ -26,14 +25,18 @@ CREATE TABLE temperatures (
     PRIMARY KEY (sensor_name, time)
 );
 
+CREATE TABLE control_log (
+    time timestamp with time zone not null primary key,
+    power double precision not null
+);
+
 GRANT SELECT ON temperatures TO "www-data";
-GRANT SELECT ON temperatures TO "daniel";
-GRANT SELECT ON cooking TO "www-data";
-GRANT SELECT ON cooking TO "sousvide";
-GRANT SELECT ON cooking TO "daniel";
+GRANT SELECT ON control_log  TO "www-data";
+GRANT SELECT ON orders       TO "www-data";
 
+GRANT SELECT ON orders       TO "sousvide";
 GRANT INSERT ON temperatures TO "sousvide";
-GRANT UPDATE ( last_updated, current_duty_cycle ) ON cooking TO "sousvide";
+GRANT INSERT ON control_log  TO "sousvide";
 
-GRANT INSERT ON cooking TO "daniel";
-GRANT SELECT, UPDATE ON cooking_seqno_seq TO "daniel";
+GRANT INSERT ON orders TO "chef";
+GRANT SELECT, UPDATE ON orders_seqno_seq TO "chef";
--- setup.md
+++ setup.md
@@ -1,4 +1,4 @@
-- Instlal postgresql-9.6 python3-psycopg2
+- apt-get install postgresql-9.6 python3-psycopg2 git
 - echo dtoverlay=w1-gpio >> /boot/config.txt
 - (echo w1-gpio; echo w1-therm) >> /etc/modules
 - sudo -u postgres psql postgres -c 'create database sousvide'
