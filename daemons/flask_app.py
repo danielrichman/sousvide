58819a227a8fc4df4e6d57266b18a030749c7cc8
Daniel Richman <main@danielrichman.co.uk>
2017-08-20 03:15:21 +0100

    x

--- daemons/flask_app.py
+++ daemons/flask_app.py
@@ -10,8 +10,12 @@ def hello():
     with sousvidedb() as db_cur:
         db_cur.execute("SET TimeZone='Europe/London'")
         db_cur.execute("""
-            SELECT control_log.power, control_log.failed, orders.name as order_name,
-                   orders.start_time, orders.end_time, orders.target_temperature
+            SELECT control_log.power         AS control_log_power,
+                   control_log.failed        AS control_log_failed,
+                   orders.name               AS order_name,
+                   orders.start_time         AS order_start_time,
+                   orders.end_time           AS order_end_time,
+                   orders.target_temperature AS order_target_temperature
             FROM control_log
             LEFT JOIN orders ON orders.order_id = control_log.in_response_to_order
             WHERE control_log.time > current_timestamp - interval '1 minute'
@@ -21,10 +25,27 @@ def hello():
 
         data = db_cur.fetchone()
 
+        db_cur.execute("""
+            SELECT reading
+            FROM temperatures
+            WHERE time > current_timestamp - interval '1 minute'
+            ORDER BY time DESC
+            LIMIT 1
+        """)
+
+        data2 = db_cur.fetchone()
+
     if data is None:
         return jsonify({"error": "state unknown"}), 400
-    else:
-        for key in ("start_time", "end_time"):
-            data[key] = str(data[key])
-        return jsonify(dict(data))
 
+    if data2 is None:
+        data["current_temperature"] = None
+    else:
+        data["current_temperature"] = data2["reading"]
+
+    # The default way these get stringed is weird. Apparently calling str()
+    # forces them into a nicer format.
+    for key in ("start_time", "end_time"):
+        data[key] = str(data[key])
+
+    return jsonify(dict(data))
